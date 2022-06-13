3fd740cebd19462b33eff9c15aed5a4815552d44
Linvay <63941955+Linvay@users.noreply.github.com>
2022-06-13 11:32:00 +0800

    feature: changed test/get to fetch all algorithm in db

--- route/test.py
+++ route/test.py
@@ -3,6 +3,8 @@ from flask import Blueprint
 from .utils.response import HTTPResponse, HTTPError
 from .utils.request import Request
 
+from db.DataBase import DataBase
+
 __all__ = ['test_api']
 
 test_api = Blueprint('test_api', __name__)
@@ -21,8 +23,11 @@ def test_post(key1, key2):
 @test_api.route('/get', methods=['GET'])
 @Request.args('val1','val2')
 def test_get(val1,val2):
+    with DataBase() as db:
+        test_data = db.get_algo_all()
     try:
-        return HTTPResponse(data={'ret1':val1,'ret2':val2},message='test success')
+        # return HTTPResponse(data={'ret1':val1,'ret2':val2},message='test success')
+        return HTTPResponse(data=test_data, message='test success')
     except:
         return HTTPError('unknown error', 406)
         
