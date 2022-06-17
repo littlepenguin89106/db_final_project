172d832d2ef2afe40dbc8ca5beb2f548e73e2f46
littlepenguin89106 <littlepenguin89106@gmail.com>
2022-06-17 11:11:45 +0800

    add waitress server

--- app.py
+++ app.py
@@ -4,19 +4,15 @@ from database.GetManager import GetManager
 # from route import api
 from route import test_api,be_api
 
-def create_app():
-    app = Flask(__name__)
-    app.config['DEBUG'] = True
+app = Flask(__name__)
+app.config['DEBUG'] = True
 
-    api2prefix = [
-        (test_api,'/test'),
-        (be_api,'/')
-    ]
-    for api, prefix in api2prefix:
-        app.register_blueprint(api, url_prefix=prefix)
-
-    return app
+api2prefix = [
+    (test_api,'/test'),
+    (be_api,'/')
+]
+for api, prefix in api2prefix:
+    app.register_blueprint(api, url_prefix=prefix)
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(host='localhost',port=5000,debug=True)
